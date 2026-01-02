a3afe72ff945b43d3201ec5d4e055d1f00796dad
Luckysun <luckysun2399@mail.ru>
2026-01-02 14:56:29 +0300

    Коммит

--- main.go
+++ main.go
@@ -4,4 +4,14 @@ func main() {
 	const usdEur = 0.854
 	const usdRub = 80.72
 	const eurRub = usdRub / usdEur
+	result := userScan(a)
+}
+
+func userScan(a string) {
+	fmt.Scan(a)
+	result a
+}
+
+func userCalculation(a int, usd int, rub int) int {
+	
 }
